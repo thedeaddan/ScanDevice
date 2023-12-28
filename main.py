afda54066dcc2346a2dccc0c444686942e69584a
thedeaddan <thedeaddan@mail.ru>
2023-12-28 18:42:43 +0300

    Восстановлена поддержка Linux

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from wmi import WMI
 from peewee import SqliteDatabase, AutoField, CharField, Model
+from psutil import virtual_memory, cpu_count, cpu_freq
 from telebot import telebot
-from psutil import virtual_memory, cpu_count
-
-
+from platform import system,uname,processor,machine
 print("Инициализация токена...")
 try:
     from config import token, user_id
@@ -13,17 +11,6 @@ except Exception as e:
 
 print("Инициализация базы данных SQLite...")
 db = SqliteDatabase('computer_info.db')
-
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-
-def bytes_to_gb(bytes_value):
-    return round(bytes_value / (1024 ** 3))
 
 class ComputerInfo(Model):
     computer_id = AutoField()
@@ -41,32 +28,57 @@ class ComputerInfo(Model):
     class Meta:
         database = db
 
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+def bytes_to_gb(bytes_value):
+    return round(bytes_value / (1024 ** 3))
 
 print("Создание таблицы, если она еще не существует...")
 db.connect()
 db.create_tables([ComputerInfo], safe=True)
 
 print("Сбор информации о системе...")
-computer = WMI()
-computer_info = computer.Win32_ComputerSystem()[0]
-os_info = computer.Win32_OperatingSystem()[0]
-proc_info = computer.Win32_Processor()[0]
-gpu_info = computer.Win32_VideoController()[0]
-disk_info = computer.Win32_DiskDrive()
+if system() == "Linux":
+    import GPUtil
+    gpu = GPUtil.getGPUs()[0]
+    uname = uname()
+    processor_name = uname.processor
+    node_name = uname.node
+    processor_cores = cpu_count(logical=False)
+    processor_threads = cpu_count(logical=True)
+    svmem = virtual_memory()
+    ram = get_size(svmem.total)
+    graphics_card_mem = f"{gpu.memoryTotal}MB"
+    graphics_card = gpu.name
+    os_name = uname.system
+    os_version = uname.version
+else:
+    from wmi import WMI
+    computer = WMI()
+    computer_info = computer.Win32_ComputerSystem()[0]
+    os_info = computer.Win32_OperatingSystem()[0]
+    proc_info = computer.Win32_Processor()[0]
+    gpu_info = computer.Win32_VideoController()[0]
+    disk_info = computer.Win32_DiskDrive()
 
-processor_name = proc_info.Name
-node_name = os_info.CSName
-processor_cores = str(cpu_count(logical=False))
-processor_threads = str(cpu_count(logical=True))
-svmem = virtual_memory()
-ram = get_size(svmem.total)
-graphics_card_mem = bytes_to_gb(int(str(gpu_info).split("AdapterRAM")[1].split(";")[0].split(" ")[2]))
-graphics_card = gpu_info.Name
+    processor_name = proc_info.Name
+    node_name = os_info.CSName
+    processor_cores = str(cpu_count(logical=False))
+    processor_threads = str(cpu_count(logical=True))
+    svmem = virtual_memory()
+    ram = get_size(svmem.total)
+    graphics_card_mem = bytes_to_gb(int(str(gpu_info).split("AdapterRAM")[1].split(";")[0].split(" ")[2]))
+    graphics_card = gpu_info.Name
 
-os_name = os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')
-os_version = f"{os_info.Version} {os_info.BuildNumber}"
+    os_name = os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')
+    os_version = f"{os_info.Version} {os_info.BuildNumber}"
 
-hard_drives = [get_size(float(disk.Size)) for disk in disk_info]
+    hard_drives = [get_size(float(disk.Size)) for disk in disk_info]
 
 ComputerInfo.create(
     node_name=node_name,
