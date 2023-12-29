7a6299a673dd74b0e56ff3d8d63c3a8b3743fcf4
Даниил <thedeaddan@mail.ru>
2023-12-29 03:23:22 +0300

    Фикс видеокарты(Снова)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from wmi import WMI
 from peewee import SqliteDatabase, AutoField, CharField, Model
+from psutil import virtual_memory, cpu_count,disk_partitions,disk_usage
 from telebot import telebot
-from psutil import virtual_memory, cpu_count
-
-
+from platform import system,uname,processor,machine
 print("Инициализация токена...")
 try:
     from config import token, user_id
@@ -13,17 +11,6 @@ except Exception as e:
 
 print("Инициализация базы данных SQLite...")
 db = SqliteDatabase('computer_info.db')
-
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-
-def bytes_to_gb(bytes_value):
-    return round(bytes_value / (1024 ** 3))
 
 class ComputerInfo(Model):
     computer_id = AutoField()
@@ -41,32 +28,71 @@ class ComputerInfo(Model):
     class Meta:
         database = db
 
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+def bytes_to_gb(bytes_value):
+    return round(bytes_value / (1024 ** 3))
 
 print("Создание таблицы, если она еще не существует...")
 db.connect()
 db.create_tables([ComputerInfo], safe=True)
 
 print("Сбор информации о системе...")
-computer = WMI()
-computer_info = computer.Win32_ComputerSystem()[0]
-os_info = computer.Win32_OperatingSystem()[0]
-proc_info = computer.Win32_Processor()[0]
-gpu_info = computer.Win32_VideoController()[0]
-disk_info = computer.Win32_DiskDrive()
+if system() == "Linux":
+    import cpuinfo
+    import subprocess
+    uname = uname()
+    processor_name = cpuinfo.get_cpu_info()['brand_raw']
+    node_name = uname.node
+    processor_cores = cpu_count(logical=False)
+    processor_threads = cpu_count(logical=True)
+    svmem = virtual_memory()
+    ram = get_size(svmem.total)
+    try:
+        gpu = subprocess.check_output(["lspci","-nnk"]).decode("utf-8")
+        gpu = [line for line in gpu.split("\n") if "VGA" in line or "3D" in line]
+        graphics_card_mem = "----"
+        graphics_card = gpu[0].split(":")[2]
+        print(graphics_card)
+    except Exception as e:
+        print(e)
+        graphics_card_mem = "---"
+        graphics_card = "---"
+    os_name = uname.system
+    os_version = uname.version
+    partitions = disk_partitions()
+    hard_drives = []
+    for part in partitions:
+        partition_usage = disk_usage(part.mountpoint)
+        size = get_size(partition_usage.total)
+        if size != 0:
+            hard_drives.append(size)
 
-processor_name = proc_info.Name
-node_name = os_info.CSName
-processor_cores = str(cpu_count(logical=False))
-processor_threads = str(cpu_count(logical=True))
-svmem = virtual_memory()
-ram = get_size(svmem.total)
-graphics_card_mem = bytes_to_gb(int(str(gpu_info).split("AdapterRAM")[1].split(";")[0].split(" ")[2]))
-graphics_card = gpu_info.Name
+else:
+    from wmi import WMI
+    computer = WMI()
+    computer_info = computer.Win32_ComputerSystem()[0]
+    os_info = computer.Win32_OperatingSystem()[0]
+    proc_info = computer.Win32_Processor()[0]
+    gpu_info = computer.Win32_VideoController()[0]
+    disk_info = computer.Win32_DiskDrive()
 
-os_name = os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')
-os_version = f"{os_info.Version} {os_info.BuildNumber}"
-
-hard_drives = [get_size(float(disk.Size)) for disk in disk_info]
+    processor_name = proc_info.Name
+    node_name = os_info.CSName
+    processor_cores = str(cpu_count(logical=False))
+    processor_threads = str(cpu_count(logical=True))
+    svmem = virtual_memory()
+    ram = get_size(svmem.total)
+    graphics_card_mem = bytes_to_gb(int(str(gpu_info).split("AdapterRAM")[1].split(";")[0].split(" ")[2]))
+    graphics_card = gpu_info.Name
+    os_name = os_info.Name.encode('utf-8').split(b'|')[0].decode('utf-8')
+    os_version = f"{os_info.Version} {os_info.BuildNumber}"
+    hard_drives = [get_size(float(disk.Size)) for disk in disk_info]
 
 ComputerInfo.create(
     node_name=node_name,
@@ -97,14 +123,11 @@ with open('computer_info.txt', 'a', encoding='utf-8') as file:
     file.write(text)
 
 print("Отправка сообщения через Telegram...")
-if bot:
-    try:
-        bot.send_message(user_id, "*Был просканирован новый компьютер!*", parse_mode="Markdown")
-        bot.send_message(user_id, text)
-    except Exception as e:
-        print(f"Ошибка отправки сообщения в Telegram: {e}")
-else:
-    print("Бот не инициализирован, отправка отменена")
+try:
+    bot.send_message(user_id, "*Был просканирован новый компьютер!*", parse_mode="Markdown")
+    bot.send_message(user_id, text)
+except Exception as e:
+    print(f"Ошибка отправки сообщения в Telegram: {e}")
 
 print("Процесс завершен.")
 input("Нажмите для выхода")
