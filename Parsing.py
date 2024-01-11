d93e8a66c3271b35dfa6398db09684d7d1f8a793
Даниил <thedeaddan@mail.ru>
2024-01-11 11:52:46 +0300

    Фикс парсера, заполнена база заново

--- Parsing.py
+++ Parsing.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from peewee import AutoField, CharField, Model, IntegerField, SqliteDatabase
 
-url = "https://technical.city/en/video/best-price-to-performance"
+url = "https://technical.city/en/video/rating"
 
 db = SqliteDatabase('databases/accessories.db')
 
@@ -29,48 +29,44 @@ driver.get(url)
 # Ждем, чтобы динамический контент успел загрузиться
 time.sleep(5)
 
-# Листаем страницу до конца или до тех пор, пока не найдем rating_position 827
-while True:
-    # Скроллим вниз с помощью JavaScript
+# Скроллим вниз с помощью JavaScript
+for i in range(0,10):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(2)  # Ждем загрузки дополнительных данных
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    new_rows = soup.find_all("tr", {"data-id": True})
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+new_rows = soup.find_all("tr", {"data-id": True})
 
-    # Проходимся по новым строкам и извлекаем данные
-    data_list = []
-    for row in new_rows:
-        rating_position = int(row.find("td", class_="rating_list_position").text.strip())
-        video_card_name = row.find_all("td")[1].find("img")["alt"] if row.find_all("td")[1].find("img") else "N/A"
-        price = int(row.find_all("td")[6].text.strip().split(" ")[0])
-        power_consumption = row.find_all("td")[7].text.strip().split(" ")[0] if row.find_all("td")[7].text.strip() else None
-        try:
-            power_consumption = int(power_consumption.replace(".",","))
-        except:
-            power_consumption = 0
-        # Проверяем, есть ли уже запись с таким rating_position
-        existing_record = VideoCards.select().where(VideoCards.rating_position == rating_position).first()
+# Проходимся по новым строкам и извлекаем данные
+data_list = []
+for row in new_rows:
+    rating_position = int(row.find("td", class_="rating_list_position").text.strip())
+    video_card_name = row.find_all("td")[1].find("img")["alt"] if row.find_all("td")[1].find("img") else "N/A"
+    price = row.find_all("td")[6].text.strip()
+    try:
+        price = int(price.split(" ")[0])
+    except:
+        price = 0
+    power_consumption = row.find_all("td")[7].text.strip().split(" ")[0] if row.find_all("td")[7].text.strip() else None
+    try:
+        power_consumption = int(power_consumption.replace(".",","))
+    except:
+        power_consumption = 0
+    # Проверяем, есть ли уже запись с таким rating_position
+    existing_record = VideoCards.select().where(VideoCards.rating_position == rating_position).first()
 
-        # Если нет, добавляем данные в список словарей
-        if not existing_record:
-            data_list.append({
-                "rating_position": rating_position,
-                "name": video_card_name,
-                "price": price,
-                "TDP": power_consumption
-            })
+    # Если нет, добавляем данные в список словарей
+    if not existing_record:
+        data_list.append({
+            "rating_position": rating_position,
+            "name": video_card_name,
+            "price": price,
+            "TDP": power_consumption
+        })
 
-        # Если найден rating_position 827, завершаем цикл
-        if rating_position == 827:
-            break
 
-    # Если rating_position 827 был найден, выходим из внешнего цикла
-    if rating_position == 827:
-        break
-
-    # Вставляем только уникальные записи в базу данных
-    VideoCards.insert_many(data_list).execute()
+# Вставляем только уникальные записи в базу данных
+VideoCards.insert_many(data_list).execute()
 
 # Закрываем драйвер после использования
 driver.quit()
