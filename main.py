ade9870649c81efce4688ce011d381ab33bbe10c
Даниил <thedeaddan@mail.ru>
2024-01-11 12:35:28 +0300

    Фикс видеокарты, новый билд [Win 11]

--- main.py
+++ main.py
@@ -175,7 +175,10 @@ if existing_computer:
     print("Аналогичный компьютер уже существует в базе данных. Не сохраняем новую запись.")
 else:
     processor_price = CPUs.get(CPUs.name == processor_name.replace("(R)","").replace("(TM)","").split("CPU")[0].split("with")[0].strip()).price
-    graphics_card_price = VideoCards.get(VideoCards.name == graphics_card).price
+    try:
+        graphics_card_price = VideoCards.get(VideoCards.name == graphics_card).price
+    except:
+        graphics_card_price = 0
     price = processor_price+graphics_card_price
     ComputerInfo.create(
         node_name=node_name,
